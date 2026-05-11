fb04587d849157be304dfefa908ddb2349331a95
ritik kumar <ritikchugh17@gmail.com>
2026-05-11 17:53:28 +0530

    Reavese an array

---
+++ 19-reveres-array.js
@@ -0,0 +1,12 @@
+let arr = [1,2,3,4,5]
+let reversearray = []
+
+for (let i = arr.length-1; i>0; i--){
+    reversearray.push(arr[i])
+}
+console.log(reversearray)
+
+// easy method
+
+let array = [10,85,65,4]
+console.log(array.reverse())
