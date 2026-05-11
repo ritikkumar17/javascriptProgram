fe60923322abf7cb2096aaeacd3d255645c719eb
ritik kumar <ritikchugh17@gmail.com>
2026-05-11 16:41:23 +0530

    7 find the greatest element

---
+++ 7-find-largest-number.js
@@ -0,0 +1,12 @@
+let a = 100;
+let b= 201;
+let c = 90;
+
+if((a>b)&& (a>c)){
+console.log(`${a} is the greatest number}`)
+}else if ((b>a) && (b>c)){
+    console.log(`${b} is the greatest number}`)
+}else{
+    console.log(`${c} is the greatest number}`)
+}
+
